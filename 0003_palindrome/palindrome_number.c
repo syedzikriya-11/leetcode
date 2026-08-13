8eac2d4d8972fe357f67dae9359725af3e715d42
Syed zikriya <zikriya636@gmail.com>
2026-08-13 22:38:14 +0530

    Add palindrome number solution

---
+++ 0003_palindrome/palindrome_number.c
@@ -0,0 +1,17 @@
+#include <stdbool.h>
+
+bool isPalindrome(int x) {
+    if (x < 0)
+        return false;
+
+    int original = x;
+    int reverse = 0;
+
+    while (x != 0) {
+        int digit = x % 10;
+        reverse = reverse * 10 + digit;
+        x = x / 10;
+    }
+
+    return original == reverse;
+}
